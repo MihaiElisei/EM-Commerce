a4798b106c879ebc38ea1476b3533f53138a02cb
Mihai <mihai.m.elisei@gmail.com>
2023-05-07 16:52:58 +0100

    added products app and models

---
+++ products/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Category, Product
+# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'category', 'updated_date',
+                    'is_available', 'rating', 'image')
+    
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ products/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254, unique=True)
+    slug = models.SlugField(max_length=254, unique=True)
+    is_popular = models.BooleanField(default=False)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254, unique=True)
+    description = models.TextField()
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
