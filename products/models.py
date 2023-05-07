a8fa861081748e779f1d7f56690125151aa8cebb
Mihai <mihai.m.elisei@gmail.com>
2023-05-07 18:49:16 +0100

    added product reviews model, form, views and template

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.db.models import Avg, Count
 # Create your models here.
 
 
@@ -37,5 +39,37 @@ class Product(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
+    def average_review(self):
+        """Calculate the average ratings of the reviews"""
+        reviews = ReviewRating.objects.filter(
+            product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
+    
+    def count_reviews(self):
+        """Count the reviews"""
+        reviews = ReviewRating.objects.filter(
+            product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
+
     def __str__(self):
         return self.name
+    
+
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=254, blank=True)
+    review = models.TextField(max_length=1024, blank=True)
+    rating = models.FloatField()
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .models import Product, ReviewRating
+from .forms import ReviewForm
 # Create your views here.
 
 
@@ -18,9 +20,37 @@ def product_detail(request, product_id):
     """ A view to show individual product details"""
 
     product = get_object_or_404(Product, pk=product_id)
-
+    # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
     context = {
         'product': product,
+        'reviews': reviews,
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def submit_review(request, product_id):
+    """A view to handle reviews for products"""
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        try:
+            reviews = ReviewRating.objects.get(
+                user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, "Thank you for your review")
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(
+                    request, "Thank you! Your review has beem submitted")
+                return redirect(url)
