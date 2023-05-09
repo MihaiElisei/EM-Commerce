88f3cbf34bdd504db2a707c6edcab561507dc099
Mihai <mihai.m.elisei@gmail.com>
2023-05-09 22:05:32 +0100

    added product variation functionality

--- cart/views.py
+++ cart/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from products.models import Product
+from products.models import Product, Variation
 from .models import Cart, CartItem
 from django.contrib import messages
-# Create your views here.
 
 
 def view_cart(request):
     """ A view that renders the cart contents page """
-    
     return render(request, 'cart/cart.html')
 
 
@@ -25,8 +23,25 @@ def add_to_cart(request, product_id):
     """ A view that manages add items to the cart
         and products variation 
      """
-    
+
     product = Product.objects.get(id=product_id)
+    product_variation = []
+
+    # Product Variations for colors and sizes
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(
+                    product=product,
+                    variation_category__iexact=key,
+                    variation_value__iexact=value,
+                )
+                product_variation.append(variation)
+            except:
+                pass
 
     try:
         # get the cart using the cart id present in the session
@@ -38,19 +53,56 @@ def add_to_cart(request, product_id):
         )
     cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product)
-        cart_item.quantity += 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
+    if product_variation:
+        messages.success(
+            request, f'Added {product.name} with color {product_variation[0]} and size {product_variation[1]} to your cart')
+    else:
+        messages.success(request, f'Added {product.name} to your cart')
+    cart_item_exists = CartItem.objects.filter(
+        product=product, cart=cart).exists()
+
+    if cart_item_exists:
+        cart_item = CartItem.objects.filter(
+            product=product,
+            cart=cart
+        )
+        existing_variation_list = []
+        id = []
+        for item in cart_item:  # If the cart item exists loop the cart item and get variations for each item
+            existing_variation = item.variations.all()
+            existing_variation_list.append(list(existing_variation))
+            id.append(item.id)
+
+        if product_variation in existing_variation_list:
+            index = existing_variation_list.index(product_variation)
+            # Get the item id of the correct cart item to increse the quantity of the correct cart item
+            item_id = id[index]
+            item = CartItem.objects.get(product=product, id=item_id)
+            item.quantity += 1
+            item.save()
+        else:
+            item = CartItem.objects.create(
+                product=product,
+                quantity=1,
+                cart=cart
+            )
+            if len(product_variation) > 0:
+                item.variations.clear()
+                item.variations.add(*product_variation)
+            item.save()
+    else:
         cart_item = CartItem.objects.create(
             product=product,
             quantity=1,
-            cart=cart,
+            cart=cart
         )
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            cart_item.variations.add(*product_variation)
         cart_item.save()
+    
     return redirect('view_cart')
-
+    
 
 def remove_from_cart(request, product_id, cart_item_id):
     """ A view to decrese items number from the cart """
