057a767b72b99388dbea4d3fb1595c07177627f7
Mihai <mihai.m.elisei@gmail.com>
2023-05-09 21:38:56 +0100

    added products variation model

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product, ReviewRating
+from .models import Category, Product, ReviewRating, Variation
 # Register your models here.
 
 
@@ -13,6 +13,14 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category',
+                    'variation_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value')
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(ReviewRating)
+admin.site.register(Variation, VariationAdmin)
